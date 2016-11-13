e81466bd5f26bd71c84e1ec161b1a15b1dbe15a7
Lucas Souza <lucas.soaresouza@gmail.com>
2016-11-12 23:40:17 -0200

    Ajustes nos menus e o menu Vestibulares-Vestibulares foi add.

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 void siteMenu();
 void styleMenu();
 void examMenu();
+void exams();
 
 
 int main(){
@@ -16,12 +17,16 @@ int main(){
 }
 
 
+
+
+//MENU PRINCIPAL DA APLICAÇÃO
+
 void siteMenu(){
 
     system("clear");
     int opcao = -1;
 
-    while(true){
+    while(opcao != 0){
 
         printf("\t\t *** Site Uol  ***\n");
         printf("1 - Estilo\n");
@@ -34,22 +39,18 @@ void siteMenu(){
         switch (opcao){
 
             case 1:
-
                 system("clear");
                 styleMenu();
                 break;
 
             case 2:
-
                 system("clear");
                 examMenu();
                 break;
 
             case 0:
-
                 system("clear");
-                printf("Sair\n");
-                exit(0);
+                //Simplesmente sai do laço voltando para a main()
                 break;
 
             default:
@@ -60,15 +61,20 @@ void siteMenu(){
         }
 
     }
-
 }
+
+
+
+
+
+// Menu ESTILO da aplicação
 
 void styleMenu(){
 
     system("clear");
     int opcao = -1;
 
-    while(true){
+    while(opcao !=  0){
 
         printf("\t\t *** Estilo  ***\n");
         printf("1 - Signos\n");
@@ -94,7 +100,7 @@ void styleMenu(){
 
             case 0:
                 system("clear");
-                siteMenu();
+                //Sai do laço e volta para a função siteMenu();
                 break;
 
             default:
@@ -108,12 +114,17 @@ void styleMenu(){
 
 }
 
+
+
+
+// Menu VESTIBULARES da aplicação
+
 void examMenu(){
 
     system("clear");
     int opcao = -1;
 
-    while(true){
+    while(opcao != 0){
 
         printf("\t\t *** Vestibular  ***\n");
         printf("1 - Faculdades / Universidades\n");
@@ -134,12 +145,12 @@ void examMenu(){
             case 2:
                 //AQUI ENTRA A FUNÇÃO RESPONSÁVEL PELOS VESTIBULARES - LUCAS SOARES E LUDE RIBEIRO
                 system("clear");
-                printf("Vestibulares\n");
+                exams();
                 break;
 
             case 0:
                 system("clear");
-                siteMenu();
+                //Sai do laço voltando para função siteMenu()
                 break;
 
             default:
@@ -152,3 +163,73 @@ void examMenu(){
     }
 
 }
+
+
+// Parte Lucas S. Souza e Lude Ribeiro
+// Vestibulares - Vestibulares
+
+void exams(){
+    system("clear");
+    int opcao = -1;
+
+    while(opcao != 0){
+
+        printf("\t\t *** Vestibulares  ***\n");
+        printf("1 - Cadastrar\n");
+        printf("2 - Listar\n");
+        printf("3 - Buscar\n");
+        printf("4 - Atualizar\n");
+        printf("5 - Excluir\n");
+        printf("0 - Voltar\n");
+        printf("\nEscolha uma opcao: ");
+        scanf("%d", &opcao);
+        __fpurge(stdin);
+
+        switch (opcao){
+
+            case 1:
+                system("clear");
+                printf("Cadastro\n");
+                break;
+
+            case 2:
+                system("clear");
+                printf("Listar\n");
+                break;
+
+            case 3:
+                system("clear");
+                printf("Buscar\n");
+                break;
+
+            case 4:
+                system("clear");
+                printf("Atualizar\n");
+                break;
+
+            case 5:
+                system("clear");
+                printf("Excluir\n");
+                break;
+
+            case 0:
+                system("clear");
+                //Sai do laço voltando para a função examsMenu()
+                break;
+
+            default:
+                system("clear");
+                printf("Opcao invalida!\n");
+                break;
+
+        }
+
+    }
+
+}
+
+
+
+// Parte Lucas Penido e Lucas Siqueira
+// Vestibulares - Faculdades/Universidades
+
