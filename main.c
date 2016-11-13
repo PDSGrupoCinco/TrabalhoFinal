0dc15d579890c91cc14db3d4f206e56ae3a9d8a3
Lucas Souza <lucas.soaresouza@gmail.com>
2016-11-12 22:07:08 -0200

    Menu Estilo e Menu Vestibular adicionados ao programa.

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <stdio_ext.h>
 
+void styleMenu();
+void examMenu();
+
 int main(){
     system("clear");
     int opcao = -1;
@@ -22,20 +25,21 @@ int main(){
             case 1:
 
                 system("clear");
-                printf("Estilo\n");
+                styleMenu();
                 break;
 
             case 2:
 
                 system("clear");
-                printf("Vestibular\n");
+                examMenu();
                 break;
 
             case 0:
 
                 system("clear");
                 printf("Sair\n");
-                return 0;
+                exit(0);
+                break;
 
             default:
                 system("clear");
@@ -47,4 +51,94 @@ int main(){
     }
 
     return 0;
+}
+
+void styleMenu(){
+
+    system("clear");
+    int opcao = -1;
+
+    while(true){
+
+        printf("\t\t *** Estilo  ***\n");
+        printf("1 - Signos\n");
+        printf("2 - Viagens\n");
+        printf("0 - Voltar\n");
+        printf("\nEscolha uma opcao: ");
+        scanf("%d", &opcao);
+        __fpurge(stdin);
+
+        switch (opcao){
+
+            case 1:
+                system("clear");
+                //AQUI ENTRA A FUNÇÃO RESPONSÁVEL PELOS SIGNOS - LUCAS KISHIMA
+                printf("Signos\n");
+                break;
+
+            case 2:
+                //AQUI ENTRA A FUNÇÃO RESPONSÁVEL PELAS VIAGENS - LUCIANA E LUCIANO
+                system("clear");
+                printf("Viagens\n");
+                break;
+
+            case 0:
+                system("clear");
+                main();
+                break;
+
+            default:
+                system("clear");
+                printf("Opcao invalida!\n");
+                break;
+
+        }
+
+    }
+
+}
+
+void examMenu(){
+
+    system("clear");
+    int opcao = -1;
+
+    while(true){
+
+        printf("\t\t *** Vestibular  ***\n");
+        printf("1 - Faculdades / Universidades\n");
+        printf("2 - Vestibulares\n");
+        printf("0 - Voltar\n");
+        printf("\nEscolha uma opcao: ");
+        scanf("%d", &opcao);
+        __fpurge(stdin);
+
+        switch (opcao){
+
+            case 1:
+                system("clear");
+                //AQUI ENTRA A FUNÇÃO RESPONSÁVEL PELA PARTE FACULDADES / UNIVERSIDADES - LUCAS PENIDO E LUCAS SIQUEIRA
+                printf("Faculdades / Universidades \n");
+                break;
+
+            case 2:
+                //AQUI ENTRA A FUNÇÃO RESPONSÁVEL PELOS VESTIBULARES - LUCAS SOARES E LUDE RIBEIRO
+                system("clear");
+                printf("Vestibulares\n");
+                break;
+
+            case 0:
+                system("clear");
+                main();
+                break;
+
+            default:
+                system("clear");
+                printf("Opcao invalida!\n");
+                break;
+
+        }
+
+    }
+
 }
