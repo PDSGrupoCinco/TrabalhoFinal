95e972b6733a3bf971c05297f58ffda913de7b4a
Lucas Souza <lucas.soaresouza@gmail.com>
2016-11-16 01:41:53 -0200

    Foi implementada a funcao App para Intercambio

--- main.c
+++ main.c
@@ -45,6 +45,26 @@ void validaMenuBusca(int *pOpcao);
 void validaTipo(char *pTipo);
 int uniFacul();
 
+// Protótipos Aplicativos para Intercâmbio
+
+void cadastroApp();
+void editaDadosApp();
+void listaApp();
+void buscaApp();
+void deletaApp();
+void validaNomeChaveApp(char *pNome);
+void validaNumApp (double *num);
+void validaOpcaoApp(int *pOpcao);
+void validaOpcao2App(int *pOpcao);
+void validaNomeApp(char *pNome);
+void validaMenuBuscaApp(int *pOpcao);
+void validaConfirmaApp(int *pComfima);
+void buscaChaveApp(char *pNome);
+void buscaPlataformaApp(char *pNome);
+void mudaLetraApp (char *palavra);
+int appIntercambio();
+
+
 // Variáveis globais
 
 int guardaPosicao = 0;
@@ -75,6 +95,19 @@ typedef struct
     int vagas;
 
 }vestibular;
+
+
+// Struct Aplicativos
+
+typedef struct
+{
+    char nome[MAX];
+    char plataforma[MAX];
+    double memoria;
+    char descricao[300];
+
+} aplicativos;
+
 
 
 int main(){
@@ -195,6 +228,7 @@ void vestibularMenu(){
         printf("\t\t *** Vestibular  ***\n");
         printf("1 - Faculdades / Universidades\n");
         printf("2 - Vestibulares\n");
+        printf("3 - Aplicativos de Auxilio a Intercambio\n");
         printf("0 - Voltar\n");
         printf("\nEscolha uma opcao: ");
         scanf("%d", &opcao);
@@ -213,7 +247,11 @@ void vestibularMenu(){
                 system("clear");
                 vestibulares();
                 break;
-
+            case 3:
+                //AQUI ENTRA A FUNÇÃO RESPONSÁVEL PELOS APLICATIVOS - LUCAS PENIDO
+                system("clear");
+                appIntercambio();
+                break;
             case 0:
                 system("clear");
                 //Sai do laço voltando para função siteMenu()
@@ -448,7 +486,8 @@ void listarVest(){
 
     while (aux3 != contador)
     {
-        printf("NOME: %s INSTITUICAO: %s UF: %s FASE: %s CHAMADA: %s VAGAS: %d VALOR: %.2fR$ DATA: %s DESCRICAO: %s", pMalloc[aux3].nome, pMalloc[aux3].instituicao, pMalloc[aux3].uf, pMalloc[aux3].fase, pMalloc[aux3].chamada, pMalloc[aux3].vagas, pMalloc[aux3].valor, pMalloc[aux3].data, pMalloc[aux3].descricao);
+        //printf("NOME: %s INSTITUICAO: %s UF: %s FASE: %s CHAMADA: %s VAGAS: %d VALOR: %.2fR$ DATA: %s DESCRICAO: %s", pMalloc[aux3].nome, pMalloc[aux3].instituicao, pMalloc[aux3].uf, pMalloc[aux3].fase, pMalloc[aux3].chamada, pMalloc[aux3].vagas, pMalloc[aux3].valor, pMalloc[aux3].data, pMalloc[aux3].descricao);
+        printf("NOME: %s ", pMalloc[aux3].nome);
         aux3++;
     }
     printf("\n");
@@ -1093,8 +1132,8 @@ int uniFacul(void)
 // Instrucoes
     while (opcao != 0)
     {
-        printf("\t\tFACULDADES\n\n");
-        printf("1. Cadastrar \n2. Listar \n3. Buscar\n4. Atualizar\n5. Deletar \n0. Voltar\n");
+        printf("\t\t*** Faculdades ***\n\n");
+        printf("1 - Cadastrar \n2 - Listar \n3 - Buscar\n4 - Atualizar\n5 - Deletar \n0 - Voltar\n");
         printf("\nEscolha a opcao que deseja: ");
         scanf("%d",&opcao);
         __fpurge(stdin);
@@ -1801,6 +1840,723 @@ void validaTipo(char *pTipo)
 }
 
 void mudaLetra(char *palavra)
+{
+    int i;
+
+    for (i=0; palavra[i]!='\0'; i++)
+    {
+        palavra[i] = toupper(palavra[i]);
+    }
+}
+
+/**********************************************************************************************************************************/
+//Vestibulares - Aplicativos de Auxilo de Intercambio
+
+int appIntercambio(void)
+{
+// Declaracoes
+    int opcao = -1;
+// Instrucoes
+    while (opcao != 0)
+    {
+        printf("\t\t *** Aplicativos para Auxilio a Intercambio ***\n\n");
+        printf("1 - Cadastrar \n2 - Listar \n3 - Buscar\n4 - Atualizar\n5 - Deletar \n0 - Voltar\n");
+        printf("\nEscolha a opcao que deseja: ");
+        scanf("%d",&opcao);
+        __fpurge(stdin);
+        validaOpcaoApp(&opcao);
+        switch (opcao)
+        {
+        case 1:
+            cadastroApp();
+            break;
+        case 2:
+            listaApp();
+            break;
+        case 3:
+            buscaApp();
+            break;
+        case 4:
+            editaDadosApp();
+            break;
+        case 5:
+            deletaApp();
+            break;
+        }
+
+    }
+    LIMPA_TELA;
+    return 0;
+}
+
+
+
+void cadastroApp()
+{
+    //Declaraões locais
+    aplicativos cadastrar;
+    FILE *pArquivo;
+    int confirma;
+
+    // Instruções
+
+    if ((pArquivo = fopen("aplicativos.txt","a+")) == NULL)
+    {
+        printf("Erro ao abrir o arquivo");
+        return;
+    }
+    LIMPA_TELA;
+
+    printf("\t\tCadastrar\n\n");
+
+    printf ("Informe o nome do Aplicativo : \n");
+    fgets(cadastrar.nome,MAX,stdin);
+    __fpurge(stdin);
+
+    validaNomeApp(cadastrar.nome);
+
+    validaNomeChaveApp(cadastrar.nome);
+
+    __fpurge(stdin);
+
+    printf("Informe a Plataforma : \n");
+    fgets(cadastrar.plataforma,MAX,stdin);
+
+    __fpurge(stdin);
+
+    validaNomeApp(cadastrar.plataforma);
+
+    __fpurge(stdin);
+
+    printf("Informe o Tamanho em MB: \n");
+
+    scanf("%lf",&cadastrar.memoria);
+
+    __fpurge(stdin);
+
+
+    validaNumApp(&cadastrar.memoria);
+
+    printf("Informe a Descricao : \n");
+
+    fgets(cadastrar.descricao,300,stdin);
+
+    __fpurge(stdin);
+
+    validaNomeApp(cadastrar.descricao);
+
+    __fpurge(stdin);
+
+    printf("\n1 - Salvar\n0 - Cancelar\n");
+    scanf("%d",&confirma);
+
+    __fpurge(stdin);
+
+    validaConfirmaApp(&confirma);
+    LIMPA_TELA;
+
+    switch(confirma)
+    {
+    case 1:
+        if (fwrite(&cadastrar,sizeof(aplicativos),1,pArquivo) == 1)
+        {
+            printf("Aplicativo cadastrado\n\n");
+        }
+        else
+        {
+            printf("Erro no cadastro");
+        }
+        break;
+    }
+
+    fclose(pArquivo);
+}
+
+void editaDadosApp()
+{
+    //Declarações:
+    aplicativos dados;
+    FILE *pArquivos;
+    FILE *arquivoNovo;
+    int escolha=0;
+    char chave[MAX];
+    int opcao2=0;
+    int confirmacao=0;
+
+    //Instruções:
+    if ((pArquivos = fopen("aplicativos.txt","r+")) == NULL)
+    {
+        LIMPA_TELA;
+        printf("Erro ao abrir o arquivo\n\n");
+        return;
+    }
+
+    //Verifica se o arquivo está vazio
+    fseek (pArquivos, 0, SEEK_END);
+    if (ftell (pArquivos) == 0)
+    {
+        LIMPA_TELA;
+        printf("Nenhum Aplicativo para atualizar\n\n");
+        return;
+    }
+    LIMPA_TELA;
+    printf("\t\tATUALIZAR\n\n");
+
+
+    printf("Informe o nome do aplicativo que deseja atualizar: \n");
+    fgets(chave,MAX,stdin);
+
+    __fpurge(stdin);
+
+    validaNomeApp(chave);
+
+    buscaChaveApp(chave);
+
+    if(guardaPosicao == 0)
+    {
+        LIMPA_TELA;
+        printf("Aplicativo inexistente!! \n");
+        return;
+    }
+    printf("\nEsse e o aplicativo que deseja atualizar?\n1 - SIM\n0 - NAO\n");
+    scanf("%d",&escolha);
+    __fpurge(stdin);
+
+    validaConfirmaApp(&escolha);
+
+    if (escolha == 0)
+    {
+        LIMPA_TELA;
+        return;
+    }
+
+    fseek(pArquivos,((guardaPosicao - 1) * sizeof(aplicativos)), SEEK_SET);
+
+    fread(&dados,sizeof(aplicativos),1,pArquivos);
+
+    while (opcao2 != 6)
+    {
+        printf("\nInforme o dado que deseja atualizar \n");
+        printf("1. Nome \n2. Plataforma \n3. Tamanho \n4. Descricao \n5. Apagar o Aplicativo \n6. Sair\n");
+        printf("Escolha a opcao que deseja: ");
+        scanf("%d",&opcao2);
+        __fpurge(stdin);
+
+        validaOpcao2App(&opcao2);
+
+        switch(opcao2)
+        {
+        case 1:
+
+            printf("Informe o novo nome \n");
+            fgets(dados.nome,MAX,stdin);
+
+            __fpurge(stdin);
+
+            validaNomeApp(dados.nome);
+
+            validaNomeChaveApp(dados.nome);
+
+            fseek(pArquivos,((guardaPosicao - 1) * sizeof(aplicativos)), SEEK_SET);
+
+            if (fwrite(&dados,sizeof(aplicativos),1,pArquivos) == 1)
+            {
+                LIMPA_TELA;
+                printf("\nAlteracao realizada com sucesso\n\n");
+                printf("Nome: %s \n",dados.nome);
+                printf("Plataforma: %s \n",dados.plataforma);
+                printf("Tamanho: %.2lf MB \n",dados.memoria);
+                printf("Descricao: %s \n",dados.descricao);
+            }
+            else
+            {
+                printf("Erro\n");
+            }
+            fclose(pArquivos);
+            return;
+            break;
+        case 2:
+            printf("Informe a nova Plataforma \n");
+            fgets(dados.plataforma,MAX,stdin);
+
+
+            __fpurge(stdin);
+
+            validaNomeApp(dados.plataforma);
+
+            fseek(pArquivos,((guardaPosicao - 1) * sizeof(aplicativos)), SEEK_SET);
+
+            if (fwrite(&dados,sizeof(aplicativos),1,pArquivos) == 1)
+            {
+                LIMPA_TELA;
+                printf("\nAlteracao realizada com sucesso\n\n");
+                printf("Nome: %s \n",dados.nome);
+                printf("Plataforma: %s \n",dados.plataforma);
+                printf("Tamanho: %lf MB \n",dados.memoria);
+                printf("Descricao: %s \n",dados.descricao);
+            }
+            else
+            {
+                printf("Erro\n");
+            }
+            fclose(pArquivos);
+            return;
+            break;
+
+        case 3:
+            printf("Informe o novo Tamanho em MB \n");
+            scanf(".2%lf",&dados.memoria);
+            __fpurge(stdin);
+            validaNumApp(&dados.memoria);
+
+
+            fseek(pArquivos,((guardaPosicao - 1) * sizeof(aplicativos)), SEEK_SET);
+
+            if (fwrite(&dados,sizeof(aplicativos),1,pArquivos) == 1)
+            {
+                LIMPA_TELA;
+                printf("\nAlteracao realizada com sucesso\n\n");
+                printf("Nome: %s \n",dados.nome);
+                printf("Plataforma: %s \n",dados.plataforma);
+                printf("Tamanho: %lf MB \n",dados.memoria);
+                printf("Descricao: %s \n",dados.descricao);
+            }
+            else
+            {
+                printf("Erro\n");
+            }
+            fclose(pArquivos);
+            return;
+            break;
+        case 4:
+            printf("Informe a nova descricao \n");
+            fgets(dados.descricao,MAX,stdin);
+
+            __fpurge(stdin);
+
+            validaNomeApp(dados.descricao);
+
+            fseek(pArquivos,((guardaPosicao - 1) * sizeof(aplicativos)), SEEK_SET);
+
+            if (fwrite(&dados,sizeof(aplicativos),1,pArquivos) == 1)
+            {
+                LIMPA_TELA;
+                printf("\nAlteracao realizada com sucesso\n\n");
+                printf("Nome: %s \n",dados.nome);
+                printf("Plataforma: %s \n",dados.plataforma);
+                printf("Tamanho: MB %.2lf \n",dados.memoria);
+                printf("Descricao: %s \n",dados.descricao);
+            }
+            else
+            {
+                printf("Erro\n");
+            }
+            fclose(pArquivos);
+            return;
+            break;
+        case 5:
+            printf("\nDeseja mesmo deletar o Aplicativo?\n1 - SIM\n0 - NAO\n");
+            scanf("%d",&confirmacao);
+            fflush(stdin);
+
+           validaConfirmaApp(&confirmacao);
+
+            switch(confirmacao)
+            {
+            case 0:
+                return;
+            }
+
+            if((arquivoNovo = fopen("copia.txt","wt")) == NULL)
+            {
+            }
+
+            rewind(pArquivos);
+
+            while(!feof(pArquivos))
+            {
+                if(fread(&dados,sizeof(aplicativos),1,pArquivos) == 1)
+                {
+                    if(strcmp(dados.nome,chave)!=0)
+                    {
+                        if(fwrite(&dados,sizeof(aplicativos),1,arquivoNovo) == 1)
+                        {
+                        }
+                    }
+                }
+            }
+
+            fclose(pArquivos);
+            fclose(arquivoNovo);
+
+            remove("aplicativos.txt");
+            rename("copia.txt","aplicativos.txt");
+            LIMPA_TELA;
+            printf("Aplicativo Excluido\n\n");
+            return;
+        }
+    }
+
+    LIMPA_TELA;
+
+    return;
+}
+
+
+void listaApp()
+{
+//Declaraões locais
+    aplicativos cadastros;
+    FILE *pArquivos;
+    int contador = 0;
+    int aux = 0;
+    int aux2 = 0;
+    int aux3 = 0;
+    int confirma;
+    aplicativos *pMalloc;
+//Instruções
+
+    LIMPA_TELA;
+    if ((pArquivos = fopen("aplicativos.txt","a+")) == NULL)
+    {
+        printf("Erro ao abrir o arquivo");
+        return;
+    }
+
+
+
+    while (fread(&cadastros,sizeof(aplicativos),1,pArquivos) == 1)
+    {
+        contador++;
+    }
+
+    if (contador == 0)
+    {
+        LIMPA_TELA;
+        printf("Nenhum Aplicativo para listar\n\n");
+        return;
+    }
+
+    rewind(pArquivos);
+
+    pMalloc = (aplicativos *)malloc(contador * sizeof(aplicativos));
+
+    while (fread(&pMalloc[aux],sizeof(aplicativos),1,pArquivos) == 1)
+    {
+        aux++;
+    }
+
+    for(aux = 0; aux < contador - 1; aux++)
+    {
+        for(aux2 = aux + 1; aux2 < contador; aux2++)
+        {
+            if (strcmp(pMalloc[aux].nome,pMalloc[aux2].nome) > 0)
+            {
+                cadastros = pMalloc[aux2];
+                pMalloc[aux2] = pMalloc[aux];
+                pMalloc[aux] = cadastros;
+            }
+        }
+    }
+
+    printf("\t\tListar\n\n");
+    printf("Nome\n");
+
+    while (aux3 != contador)
+    {
+        printf("\n%s\n",pMalloc[aux3].nome);
+        aux3++;
+    }
+    printf("\n");
+
+    printf("1 - Voltar\n");
+    scanf("%d",&confirma);
+
+    __fpurge(stdin);
+
+    while(confirma != 1)
+    {
+        printf("Opcao invalida digite novamente \n");
+        scanf("%d",&confirma);
+        __fpurge(stdin);
+    }
+
+    LIMPA_TELA;
+
+    fclose(pArquivos);
+
+}
+
+void buscaApp()
+{
+    FILE *pArquivos;
+    int escolha;
+    int confirma;
+    char busca[MAX];
+
+    //Instruções:
+    if ((pArquivos = fopen("aplicativos.txt","r+")) == NULL)
+    {
+        LIMPA_TELA;
+        printf("Erro ao abrir o arquivo\n\n");
+        return;
+    }
+
+    //Verifica se o arquivo está vazio
+    fseek (pArquivos, 0, SEEK_END);
+    if (ftell (pArquivos) == 0)
+    {
+        LIMPA_TELA;
+        printf("Nenhum Aplicativo para buscar\n\n");
+        return;
+    }
+
+    LIMPA_TELA;
+    printf("\t\tBUSCAR\n\n");
+    printf("Busca por:\n 1-Nome \n 2-Plataforma \n 3-Sair\n");
+    printf("Escolha a opcao que deseja: ");
+    scanf("%d",&escolha);
+    validaMenuBuscaApp(&escolha);
+    __fpurge(stdin);
+    switch(escolha)
+    {
+    case 1:
+        printf("informe o nome: \n");
+        fgets(busca,MAX,stdin);
+        __fpurge(stdin);
+        validaNomeApp(busca);
+        LIMPA_TELA;
+        buscaChaveApp(busca);
+        break;
+
+    case 2:
+        printf("informe a Plataforma: \n");
+        fgets(busca,MAX,stdin);
+        __fpurge(stdin);
+        validaNomeApp(busca);
+        LIMPA_TELA;
+        buscaPlataformaApp(busca);
+        break;
+
+    case 3:
+        LIMPA_TELA;
+        return;
+    }
+
+    printf("1 - Voltar\n");
+    scanf("%d",&confirma);
+
+    __fpurge(stdin);
+
+    while(confirma != 1)
+    {
+        printf("Opcao invalida digite novamente \n");
+        scanf("%d",&confirma);
+        __fpurge(stdin);
+    }
+
+    LIMPA_TELA;
+}
+
+
+void deletaApp()
+{
+    //Declaração locais
+    int escolhe;
+    //Instruções
+    LIMPA_TELA;
+    printf("\t\tDeletar\n\n");
+    printf("Deseja excluir todos os Aplicativos cadastrados?\n1 - SIM\n0 - NAO\n");
+    scanf("%d",&escolhe);
+
+    __fpurge(stdin);
+
+    validaConfirmaApp(&escolhe);
+
+    switch (escolhe)
+    {
+    case 1:
+        remove("aplicativos.txt");
+        LIMPA_TELA;
+        printf("Arquivo excluido com sucesso. \n");
+        break;
+    case 0:
+        LIMPA_TELA;
+        printf("Exclusão de arquivo cancelada. \n");
+    }
+
+    return;
+}
+
+void validaNomeChaveApp(char *pNome)
+{
+    FILE *pArquivo;
+    aplicativos cadastro;
+
+    if ((pArquivo = fopen("aplicativos.txt","rt")) == NULL)
+    {
+        printf("Erro ao abrir o arquivo");
+        return;
+    }
+
+    while (!feof(pArquivo))
+    {
+        if ((fread(&cadastro,sizeof(aplicativos),1,pArquivo)) == 1)
+        {
+            if (strcmp(pNome,cadastro.nome) == 0)
+            {
+                printf("Nome ja cadastrada, digite um nome novo \n");
+                fgets(pNome,MAX,stdin);
+                validaNomeApp(pNome);
+                rewind(pArquivo);
+            }
+        }
+    }
+
+    fclose(pArquivo);
+
+}
+
+void validaNumApp(double *num)
+{
+    while(*num<=0)
+    {
+        printf("Digite um tamanho valido\n");
+        scanf("%lf",num);
+    }
+}
+
+void validaOpcaoApp(int *pOpcao)
+{
+
+    while(*pOpcao < 0 || *pOpcao > 5)
+    {
+        printf("Opcao invalida digite novamente \n");
+        scanf("%d",pOpcao);
+        __fpurge(stdin);
+
+    }
+}
+
+void validaOpcao2App(int *pOpcao)
+{
+
+    while(*pOpcao < 1 || *pOpcao > 5)
+    {
+        printf("Opcao invalida digite novamente \n");
+        scanf("%d",pOpcao);
+        __fpurge(stdin);
+
+    }
+}
+
+void validaNomeApp(char *pNome)
+{
+
+
+
+    while(strlen(pNome) == 1)
+    {
+        printf("Nome invalido, digite um nome valido\n");
+        fgets(pNome,MAX,stdin);
+
+        __fpurge(stdin);
+    }
+    if (pNome[strlen(pNome) - 1] == '\n')
+    {
+        pNome[strlen(pNome) -1  ] = '\0';
+    }
+    mudaLetraApp(pNome);
+}
+
+void validaMenuBuscaApp(int *pOpcao)
+{
+
+    while(*pOpcao < 1 || *pOpcao > 4)
+    {
+        printf("Opcao invalida digite novamente \n");
+        scanf("%d",pOpcao);
+        __fpurge(stdin);
+
+    }
+}
+
+void validaConfirmaApp(int *pConfirma)
+{
+
+    while(*pConfirma != 1 && *pConfirma != 0)
+    {
+        printf("Opcao invalida digite novamente \n");
+        scanf("%d",pConfirma);
+        __fpurge(stdin);
+    }
+}
+
+void buscaChaveApp(char *pNome)
+{
+    FILE *pArquivo;
+    aplicativos cadastro;
+    int contador = 0;
+    guardaPosicao = 0;
+
+
+    if ((pArquivo = fopen("aplicativos.txt","rt")) == NULL)
+    {
+        printf("Erro ao abrir o arquivo");
+        return;
+    }
+
+    while (!feof(pArquivo))
+    {
+        contador++;
+        if ((fread(&cadastro,sizeof(aplicativos),1,pArquivo)) == 1)
+        {
+            if (strcmp(pNome,cadastro.nome) == 0)
+            {
+                printf("\nNome: %s \n",cadastro.nome);
+                printf("Plataforma: %s \n",cadastro.plataforma);
+                printf("Tamanho: %.2f MB \n",cadastro.memoria);
+                printf("Descricao %s \n",cadastro.descricao);
+                guardaPosicao = contador;
+            }
+        }
+    }
+    fclose(pArquivo);
+}
+
+void buscaPlataformaApp(char *pNome)
+{
+    FILE *pArquivo;
+    aplicativos cadastro;
+    int contador = 0;
+    guardaPosicao = 0;
+
+
+    if ((pArquivo = fopen("aplicativos.txt","rt")) == NULL)
+    {
+        printf("Erro ao abrir o arquivo");
+        return;
+    }
+
+    while (!feof(pArquivo))
+    {
+        contador++;
+        if ((fread(&cadastro,sizeof(aplicativos),1,pArquivo)) == 1)
+        {
+            if (strcmp(pNome,cadastro.plataforma) == 0)
+            {
+                printf("\nNome: %s \n",cadastro.nome);
+                printf("Plataforma: %s \n",cadastro.plataforma);
+                printf("Tamanho: %.2f MB \n",cadastro.memoria);
+                printf("Descricao %s \n",cadastro.descricao);
+                guardaPosicao = contador;
+            }
+        }
+    }
+    fclose(pArquivo);
+}
+
+void mudaLetraApp(char *palavra)
 {
     int i;
 
